3b9b676310423076436350c074f26514ac0e4aaa
Krish Bhavsar <bhavsarkrish818@gmail.com>
2025-10-13 21:21:13 +0530

    Update auth.js

--- service/auth.js
+++ service/auth.js
@@ -1,17 +1,18 @@
 const jwt = require('jsonwebtoken');
 
+
 function setUser(user) {
   return jwt.sign({
       _id: user._id,
       email: user.email,
-  }, secret, { expiresIn: '1d' });
+  }, process.env.JWT_SECRET, { expiresIn: '1d' });
 }
 
 
 function getUser(token) {
     if (!token) return null;
     try {
-        const payload = jwt.verify(token, secret);
+        const payload = jwt.verify(token, process.env.JWT_SECRET);
         return payload;
     } catch (error) {
         return null;
